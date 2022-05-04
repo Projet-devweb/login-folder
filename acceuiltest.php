072b0b532d6bd06b651ff8e30f7736332906db6c
BOupdown <75842770+BOupdown@users.noreply.github.com>
2022-05-04 16:27:36 +0200

    Add files via upload

---
+++ acceuiltest.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>accueil</title>
+    </head>
+    <body>
+		<form id="form" name="form" action="consultation.php" method="POST">
+			<h1>Inscription Elève</h1>
+
+			<p>Login : <input type="text" name="login" /></p>
+
+			<input type="submit" id="submit" name="submit" value="valider">
+			<input type="reset">
+		</form>
+    </body>
+</html>
---
+++ consultation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Consultation</title>
+    </head>
+    <body>
+		<?php
+				if (($handle = fopen("annex1.csv", "r")) !== FALSE) {
+					while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+						if($_POST["login"] == $data[2]){
+						        echo "Votre moyenne générale est de : $data[4]";
+                    echo "Votre score ECTS est : $data[3]";
+                    echo "Vos choix d'option sont : $data[5] , $data[6] , $data[7] , $data[8] , $data[9] , $data[10], $data[11] , $data[12]";
+						}
+					}
+					fclose($handle);
+				}
+		?>
+
+    <?php
+    $newCsvData = array();
+    if (($handle = fopen("annex1.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $data[] = 'New Column';
+            $newCsvData[] = $data;
+        }
+        fclose($handle);
+    }
+
+    $handle = fopen('test.csv', 'w');
+
+    foreach ($newCsvData as $line) {
+       fputcsv($handle, $line);
+    }
+
+    fclose($handle);
+
+    ?>
+
+	</body>
+</html>
+<script type="text/javascript">
+  accueil
+</script>
